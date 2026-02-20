ef609f0ea6577a3569f5780341d0815cb9510478
Tim Hughes <tkhughes@MacBookPro.lan>
2026-02-20 15:17:07 -0500

    Enable auto preview via Netlify

--- booking-links.js
+++
@@ -1,6 +0,0 @@
-window.BOOKING_LINKS = {
-  americas: "https://example.com/americas",
-  emea: "https://example.com/emea",
-  apac: "https://example.com/apac",
-  worldwide: "https://example.com/worldwide"
-};
